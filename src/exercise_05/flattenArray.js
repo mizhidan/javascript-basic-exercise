20d927fd6f8494d685e7971dbf70e5f2654bc367
unknown <dj9824@126.com>
2020-07-28 22:50:41 +0800

    [danjie] refactor: flatten array

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,20 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const arrayStack = [...array];
+  const res = [];
+  while (arrayStack.length) {
+    const temp = arrayStack.pop();
+    if (temp instanceof Array) {
+      while (temp.length) {
+        res.push(temp.pop());
+      }
+    } else {
+      res.push(temp);
+    }
+  }
+  return res.reverse();
 }
