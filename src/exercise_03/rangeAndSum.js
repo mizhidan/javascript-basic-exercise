7b438ae3d5e592f787ac24e00ad9f8c8e06748f1
unknown <dj9824@126.com>
2020-07-28 18:11:05 +0800

    [danjie] feat: range_and_array_spec.js.

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -27,5 +27,5 @@ export function sum(...numbers) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  return numbers.reduce((acc, val) => acc + val, 0);
 }
