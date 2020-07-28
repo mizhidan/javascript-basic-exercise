93273f98e70cb2423f6864325f38f1f9cea65b83
unknown <dj9824@126.com>
2020-07-28 22:17:00 +0800

    [danjie] feat: array-to-list

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,15 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) {
+    throw new Error('Creating list from undefined array');
+  } else if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  return createlist(array);
+
+  function createlist(array) {
+    return array.length > 1 ? { value: array[0], next: createlist(array.slice(1))} : {value: array[0], next: null };
+  }
+  
 }
